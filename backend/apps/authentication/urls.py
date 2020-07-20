006d346bef92adb4c32e74ec172da2fc78797557
erivanio <erivanio.vanin@gmail.com>
2020-07-20 17:21:25 -0300

    add username in jwt token

--- backend/apps/authentication/urls.py
+++ backend/apps/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
+from .views import ObtainTokenPairWithUsernameView
 
 urlpatterns = [
-    path('token/obtain/', jwt_views.TokenObtainPairView.as_view(),
-         name='token_create'),  # override sjwt stock token
+    path('token/obtain/', ObtainTokenPairWithUsernameView.as_view(),
+         name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(),
          name='token_refresh'),
     path('token/verify/', jwt_views.TokenVerifyView.as_view(),
